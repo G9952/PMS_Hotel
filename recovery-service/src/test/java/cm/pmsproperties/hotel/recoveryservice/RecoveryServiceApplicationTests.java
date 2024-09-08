a1222ad4ab431ca77124ca89278dd9b3df36e3d1
G9952 <119457552+G9952@users.noreply.github.com>
2024-09-08 12:01:06 +0100

    first commit of pms hotel project

---
+++ recovery-service/src/test/java/cm/pmsproperties/hotel/recoveryservice/RecoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cm.pmsproperties.hotel.recoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
